bff9c74a3c9015ca99ba58b069840da9c1bd3323
Ermias80 <ermiasmaz8069@gmail.com>
2024-07-11 13:44:07 -0700

    all

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""
+Fabric script (based on the file 2-do_deploy_web_static.py) that creates and
+distributes an archive to your web servers, using the function deploy
+"""
+
+from fabric.api import env, local, put, run
+import os.path
+from datetime import datetime
+
+env.hosts = ['54.227.198.179', '52.91.137.24']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder.
+    Returns the archive path if successful, None otherwise.
+    """
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if os.path.isdir("versions") is False:
+            local("mkdir versions")
+        archive_path = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to the web servers
+    """
+    if os.path.isfile(archive_path) is False:
+        return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    print('New version deployed!')
+    return True
+
+
+def deploy():
+    """
+    Calls do_pack() function and store the path of the created archive.
+    Returns False if no archive has been created.
+    Calls do_deploy(archive_path) function using the new path of the new
+    archive returns the return value of do_deploy.
+    """
+    archive_path = do_pack()
+    if not archive_path:
+        return False
+    return do_deploy(archive_path)
